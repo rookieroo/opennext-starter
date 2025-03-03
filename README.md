979e33b14eac22f34bc98a1ae1c60e4e0b6438c8
Din <13716066567@163.com>
2025-03-03 10:47:52 +0800

    feat

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # opennext-starter
 opennextjs turbo saas starter
+
+> This is simple combination of [cloudflare-workers-nextjs-saas-template](https://github.com/LubomirGeorgiev/cloudflare-workers-nextjs-saas-template) with [turborepo-shadcn-nextjs](https://github.com/gmickel/turborepo-shadcn-nextjs)
+
+## FYI
+
+- no need github actions. just connect github project in the cloudflare workers
+- all secrets uploaded from local
+- monorepo for opennextjs
--- apps/web/cloudflare-env.d.ts
+++ apps/web/cloudflare-env.d.ts
@@ -6,10 +6,6 @@ interface CloudflareEnv {
 	EMAIL_FROM_NAME: "NotionFlare Team";
 	EMAIL_REPLY_TO: "useone.online@gmail.com";
 	NEXT_PUBLIC_TURNSTILE_SITE_KEY: "0x4AAAAAAA9kOm1yylE_rjIE";
-	NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PLAN_ID: "price_1Qd5AbKl4eZ0Kp65p1gi2Ir7";
-	NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID: "price_1QgkC0Kl4eZ0Kp65yx0EDvSE";
-	NEXT_PUBLIC_STRIPE_BUSINESS_MONTHLY_PLAN_ID: "price_1Qd6rwKl4eZ0Kp65NWGNrtpF";
-	NEXT_PUBLIC_STRIPE_BUSINESS_YEARLY_PLAN_ID: "price_1QgkCIKl4eZ0Kp65GOEiwycm";
 	NEXT_PUBLIC_APP_URL: "https://notionflare.com";
 	NEXT_PUBLIC_GITHUB_REPO_URL: "https://notionflare.com";
 	NEXT_PUBLIC_SITE_DESCRIPTION: "NotionFlare is a platform that allows you to create websites using Notion as a CMS.";
